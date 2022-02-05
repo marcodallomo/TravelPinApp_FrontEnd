efcba00c133dc1f1ae09401b9ca5ce8dea257146
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2022-02-05 11:37:21 +0000

    amended app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ import axios from "axios";
 import Register from "./components/Register";
 import Login from "./components/Login";
 
+///new
+import "mapbox-gl/dist/mapbox-gl.css";
+
+// added the following 6 lines.
+import mapboxgl from "mapbox-gl";
+
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+
+///new
+
 function App() {
   const myStorage = window.localStorage;
   const [currentUser, setCurrentUser] = useState(myStorage.getItem("user"));
